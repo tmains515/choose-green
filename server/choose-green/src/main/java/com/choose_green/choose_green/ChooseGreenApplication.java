69d1cfdb6b2231751a0b1f9a80d103787fddd496
Tyler <t.mains515@hotmail.com>
2025-01-30 10:09:39 -0800

    Initialized Spring Server

---
+++ server/choose-green/src/main/java/com/choose_green/choose_green/ChooseGreenApplication.java
@@ -0,0 +1,13 @@
+package com.choose_green.choose_green;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChooseGreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChooseGreenApplication.class, args);
+	}
+
+}
---
+++ server/choose-green/src/test/java/com/choose_green/choose_green/ChooseGreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choose_green.choose_green;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChooseGreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
